0bef0a4d2bbe016f1a85dc5d021f894b715417ac
Ayushmaan Shukla <102596631+ayushmaan-sh@users.noreply.github.com>
2024-01-15 21:33:51 +0530

    Update CurrencyConverter.js

--- Projects/CurrencyConverter.js
+++ Projects/CurrencyConverter.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
 const country = {
     AED: "AE",
@@ -164,6 +164,10 @@ const country = {
   
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button")
+const fromCurr = document.querySelector(".from select")
+const toCurr = document.querySelector(".to select");
+const message = document.querySelector(".message")
 
 for(let select of dropdowns){
     for(currCode in country){
@@ -191,3 +195,23 @@ const updateFlag = (element)=>{
     let img = element.parentElement.querySelector("img");
     img.src = newSrc
 }
+
+btn.addEventListener("click", async (evt)=>{
+    evt.preventDefault();
+    let amount =  document.querySelector(".amount input")
+        let amtVal = amount.value;
+        if(amtVal === "" || amtVal < 1){
+            amtVal = 1;
+            amount.value = "1";
+        }
+
+        const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+        // console.log(fromCurr.value, toCurr.value)
+
+        let response = await fetch(URL);
+        let data = await response.json();
+        let rate = data[toCurr.value.toLowerCase()];
+        
+        let finalAmount = amtVal * rate;
+        message.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+})
