896feb58785b5fd6b1dcd10741f4135f12fb524a
Ayushmaan Shukla <102596631+ayushmaan-sh@users.noreply.github.com>
2024-01-16 14:57:11 +0530

    Error handling in javascript

--- Javascript/try&catch.js
+++ Javascript/try&catch.js
@@ -1,6 +1,21 @@
-let a = prompt("enter first number")
+const ErrText = document.querySelector("p");
 
-let b = prompt("enter second number")
+const Err = () => {
+  let a = prompt("Enter the first number");
+  let b = prompt("Enter the second number");
 
-let sum = parseInt(a) + parseInt(b);
-console.log("Sum is : "+sum)
+  try {
+    if (isNaN(a) || isNaN(b)) {
+      throw new SyntaxError("Please enter a number");
+    }
+
+    let sum = parseInt(a) + parseInt(b);
+    console.log("Sum is: " + sum);
+    return "No errors";
+  } catch (error) {
+    console.error("Error:", error.message);
+    return error.message;
+  }
+};
+
+ErrText.innerText = Err();
