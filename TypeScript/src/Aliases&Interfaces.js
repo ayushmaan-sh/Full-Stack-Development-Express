44bd6464f560db109bf269d81c744288d36c0d6e
Ayushmaan Shukla <102596631+ayushmaan-sh@users.noreply.github.com>
2024-02-10 00:49:42 +0530

    Aliases and Interfaces in TypeScript

---
+++ TypeScript/src/Aliases&Interfaces.js
@@ -0,0 +1,28 @@
+"use strict";
+const employeeName = 'Ayushmaan Shukla';
+const employeeAge = 20;
+const employeeRole = 'Software Engineer';
+const employeeCompany = 'EPAM Systems';
+const details = {
+    name: employeeName,
+    age: employeeAge,
+    role: employeeRole,
+    Company: employeeCompany
+};
+console.log(details);
+const carName = 'Tesla';
+const carYear = 2015;
+const carModel = 'X';
+const car = {
+    Name: carName,
+    year: carYear,
+    model: carModel
+};
+console.log(car);
+
+
+const rectangle = {
+    height: 20,
+    width: 10
+};
+console.log(rectangle);
