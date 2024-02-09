b607565412558a7220cbb926e663cfbf4667287b
Ayushmaan Shukla <102596631+ayushmaan-sh@users.noreply.github.com>
2024-02-09 19:40:52 +0530

    Add files via upload

---
+++ TypeScript/dist/Arrays.js
@@ -0,0 +1,13 @@
+"use strict";
+{
+    const names = [];
+    names.push("Ayushmaan", "Raj", "Mayank");
+    console.log(names);
+}
+{
+    const names = ['Ayushmaan', 'Raj', 'Mayank'];
+}
+{
+    const names = ['Ayush', 'Raj', 'Mayank'];
+    names.push('aman');
+}
