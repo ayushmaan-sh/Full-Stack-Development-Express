0d00b24cddc52ce806868b0d80db6b3c13dbca64
Ayushmaan Shukla <102596631+ayushmaan-sh@users.noreply.github.com>
2024-02-14 18:48:03 +0530

    Casting

---
+++ TypeScript/dist/Casting.js
@@ -0,0 +1,10 @@
+"use strict";
+let x = 12948;
+console.log(x);
+console.log(x.length);
+let y = 'Hello TypeScript';
+console.log(y.length);
+{
+    let x = 'Hello TypeScript';
+    console.log('Example of force casting : ' + x.length);
+}
