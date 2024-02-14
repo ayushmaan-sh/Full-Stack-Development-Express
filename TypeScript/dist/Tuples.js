4f214273dd4abc4f966d313a3bbd775b5c75f09d
Ayushmaan Shukla <102596631+ayushmaan-sh@users.noreply.github.com>
2024-02-14 19:13:03 +0530

    Tuples

---
+++ TypeScript/dist/Tuples.js
@@ -0,0 +1,19 @@
+"use strict";
+{
+    let outTuple;
+    outTuple = ['Ayushmaan', 20, true];
+    outTuple.push(23);
+}
+{
+    let outTuple;
+    outTuple = ['Ayushmaan', 20, true];
+}
+{
+    const graph = [55.2, 34.4];
+    console.log(graph);
+}
+{
+    const graph = [55.2, "41.3"];
+    const [x, y] = graph;
+    console.log(x, y);
+}
