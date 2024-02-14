3209ed8498179a43326f5b19a57a08dd414fbdb2
Ayushmaan Shukla <102596631+ayushmaan-sh@users.noreply.github.com>
2024-02-14 19:11:45 +0530

    Object Types

---
+++ TypeScript/dist/ObjectTypes.js
@@ -0,0 +1,29 @@
+"use strict";
+{
+    const car = {
+        name: 'Tesla',
+        model: 'X',
+        year: 2017
+    };
+    console.log(car);
+}
+{
+    const car = {
+        type: "Toyota",
+    };
+    car.type = "Ford";
+}
+{
+    const car = {
+        name: "Tesla",
+        year: 2015,
+        type: "Electric"
+    };
+    console.log(car);
+}
+{
+    const nameAgeMap = {};
+    nameAgeMap.ayush = 20;
+    nameAgeMap.raj = 20;
+    console.log(nameAgeMap);
+}
