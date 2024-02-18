ecee76684725267cb9680115b110a60d30eccf8b
Ayushmaan Shukla <102596631+ayushmaan-sh@users.noreply.github.com>
2024-02-18 23:10:54 +0530

    Main react app file

---
+++ ReactJs/src/App.js
@@ -0,0 +1,14 @@
+import About from "./screens/About"
+import Contact from "./screens/Contact";
+import './App.css';
+
+function App() {
+  return (
+  <div>
+    <About />
+    <Contact />
+  </div>
+  );
+}
+
+export default App;
