91a37c040474728b8a904ed1150f2a93f2f35669
Ayushmaan Shukla <102596631+ayushmaan-sh@users.noreply.github.com>
2024-02-19 21:33:34 +0530

    Add Unordered lists component

---
+++ ReactJs/src/components/UL_list.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function UL_list() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About us</li>
+        <li>Log In</li>
+        <li>Sign Up</li>
+      </ul>
+    </div>
+  )
+}
+
+export default UL_list
